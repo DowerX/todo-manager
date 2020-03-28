b6e9d03a6a5606c0687c1549e7281963a6709b7f
Benedek László <41867355+DowerX@users.noreply.github.com>
2020-03-28 16:28:18 +0100

    Rewritten
    completely new

--- todo.py
+++ todo.py
@@ -1,128 +1,210 @@
-import json
-import sys
-import os
+import yaml
 import datetime
+import enum
+import os
+import sys
 
-jobs = []
-file_path = ""
-autosave = True
-dateformat = "%Y/%m/%d %H:%M"
+class Status(enum.Enum):
+    PENDING = 1
+    STARTED = 2
+    COMPLETED = 3
 
-def loadJobs(path):
-    global jobs
-    jobs = []
-    data = ""
-    try:
-        with open(path, "r") as f:
-            data = json.loads(f.read())
-        for i in data:
-            jobs.append(i)
-    except:
-        pass
-    jobs = sorted(jobs, key=lambda i: i["deadline"])
+class Job:
+    name = ""
+    description = ""
+    deadline = None
+    tags = []
+    status = Status.PENDING
 
-def saveJobs(path):
-    try:
-        os.remove(path)
-    except:
-        pass
+    def Create(self, name, description, deadline, tags, status):
+        self.name = name
+        self.description = description
+        self.deadline = deadline
+        self.tags = tags
+        self.status = status
+        return self
 
-    with open(path, "w") as f:
-        json_data = json.dumps(jobs)
-        f.write(json_data)
-
-def searchTags(tags):
-    global jobs
-    idx = []
-    for x in range(len(jobs)):
-        j = jobs[x]
-        found = True
-        for t in tags:
-            if not t in j["tags"]:
-               found = False 
-               break
-        if found:
-            idx.append(x)
-    return idx
-
-def interface():
-    global file_path, dateformat
-    print("Todo manager")
-    while True:
-        inp = input(">")
-        inp_list = inp.strip().split(" ")
-        if inp_list[0] == "add":
-            add()
-        if inp_list[0] == "remove" or inp_list[0] == "done":
-            remove(int(inp_list[1])-1)
-        elif inp_list[0] == "list":
-            listJobs()
-        elif inp_list[0] == "detail":
-            detail(int(inp_list[1])-1)
-        elif inp_list[0] == "file":
-            file_path = inp_list[1]
-            loadJobs(file_path)
-        elif inp_list[0] == "dateformat":
-            dateformat = inp_list[1]
-            loadJobs(file_path)
-        elif inp_list[0] == "exit" or inp_list[0] == "close" or inp_list[0] == "q":
-            return
-
-def add():
-    global jobs
-    job = {}
-    job["name"] = input("Name: ")
-    job["description"] = input("Description: ")
-    deadline_raw = input("Deadline: ")
-    deadline_raw = deadline_raw.strip()
-    job["deadline"] = datetime.datetime.strptime(deadline_raw, dateformat).__str__()
-    tags_raw = input("Tags: ")
-    job["tags"] = tags_raw.strip().split(",")
-
-    jobs.append(job)
-    jobs = sorted(jobs, key=lambda i: i["deadline"])
-    if autosave:
-        saveJobs(file_path)
-
-def remove(i):
-    global jobs
-    jobs.pop(i)
-    if autosave:
-        saveJobs(file_path)
-
-def save(inp_list):
-    if len(inp_list) > 1:
-        saveJobs(inp_list[1])
-    else:
-        saveJobs(file_path)
-
-def listJobs():
-    for i, job in enumerate(jobs):
-        print(f"{i+1} | {job['name']} : {job['deadline']}")
-
-def detail(i):
-    global jobs
-    job = jobs[i]
-    print(f'{job["name"]}:\n{job["description"]}, Deadline: {job["deadline"]}\nTags: {job["tags"]}')
-
-def main():
-    global file_path, autosave
-    for i, arg in enumerate(sys.argv):
-        if arg == "-f":
-            try:
-                file_path = sys.argv[i+1]
-            except:
-                print("Wrong args!")
-        elif arg == "-na":
-            autosave = False
-
-    if not file_path == "":
+    def Load(self, data):
         try:
-            loadJobs(file_path)
+            self.name = data["name"]
+            self.description = data["description"]
+            self.deadline = data["deadline"]
+            self.tags = data["tags"]
+            self.status = data["status"]
         except:
-            print("Couldn't load in file!")
-    
-    interface()
+            print("Error while loading Job.")
+
+    def Dump(self):
+        data = {
+            "name" : self.name,
+            "description" : self.description,
+            "deadline" : self.deadline,
+            "tags" : self.tags,
+            "status" : self.status
+        }
+        return data
+
+class TodoList:
+    loadedpath = ""
+    jobs = []
+
+    def Load(self, path):
+        try:
+            if os.path.exists(path):
+                self.loadedpath = path
+                self.jobs = []
+                with open(self.loadedpath, "r") as f:
+                    data = yaml.load(f.read(), Loader=yaml.CLoader)
+                    for jobdata in data:
+                        job = Job()
+                        job.Load(jobdata)
+                        self.jobs.append(job)
+            self.Sort()
+        except:
+            print("Error while loading TodoList.")
+
+    def Save(self, path):
+        try:
+            data = []
+            for j in self.jobs:
+                data.append(j.Dump())
+
+            with open(path, "w+") as f:
+                f.write(yaml.dump(data, Dumper=yaml.CDumper))
+                self.loadedpath = path
+        except:
+            print("Error while saving to file.")
+
+    def SearchTags(self, tgs):
+        idx = []
+        for i, j in enumerate(self.jobs):
+            found = True
+            for x in tgs:
+                if not x in j.tags:
+                    found = False
+            if found:
+                idx.append(i)
+        return idx
+
+    def Sort(self):
+        self.jobs = sorted(self.jobs, key=lambda i: i.deadline)
+
+class Interface:
+
+    autosave = True
+    dateformat = "%Y/%m/%d %H:%M"
+    todolist = TodoList()
+
+    def Run(self):
+        running = True
+        while running:
+            try:
+                args = input("> ").strip().split(" ")
+                if "exit" == args[0]:
+                    running = False
+                elif "add" == args[0]:
+                    self.cmdAdd()
+                elif "remove" == args[0]:
+                    self.cmdRemove(int(args[1]))
+                elif "edit" == args[0]:
+                    self.cmdEdit()
+                elif "save" == args[0]:
+                    self.cmdSave(args)
+                elif "autosave" == args[0]:
+                    if args[1] == "true":
+                        self.autosave = True
+                    else:
+                        self.autosave = False
+                elif "file" == args[0]:
+                    self.cmdFile(args[1])
+                elif "list" == args[0]:
+                    self.cmdList()
+                elif "search" == args[0]:
+                    self.cmdSearch(args)
+                elif "detail" == args[0]:
+                    self.cmdDetail(int(args[1]))
+            except:
+                print("Wrong arguments.")
+
+    def cmdAdd(self):
+        name = input("Name: ")
+        description = input("Description: ")
+        deadline = datetime.datetime.strptime(input("Deadline: "), self.dateformat)
+        tags = input("Tags: ").lstrip().rstrip().split(" ")
+        status = Status(int(input("Status: ")))
+
+        job = Job().Create(name, description, deadline, tags, status)
+        self.todolist.jobs.append(job)
+        self.todolist.Sort()
+        self.Autosave()
+
+    def cmdRemove(self, i):
+        self.todolist.jobs.pop(i)
+        self.todolist.Sort()
+        self.Autosave()
+
+    def cmdEdit(self):
+        i = int(input("Index of job you want to edit: "))
+        job = self.todolist.jobs[i]
+        print(0, "| Nothing, we are done here")
+        print(1, "| Name:", job.name)
+        print(2, "| Description:", job.description)
+        print(3, "| Deadline:", job.deadline)
+        print(4, "| Tags:", job.tags)
+        print(5, "| status:", job.status.name)
+
+        done = False
+        while not done:
+            c = int(input("Which one you want to edit: "))
+            if c == 1:
+                job.name = input("Name: ")
+            elif c == 2:
+                job.description = input("Description: ")
+            elif c == 3:
+                job.deadline = datetime.datetime.strptime(input("Deadline: "), self.dateformat)
+            elif c == 4:
+                job.tags = input("Tags: ").lstrip().rstrip().split(" ")
+            elif c == 5:
+                job.status = Status(int(input("Status: ")))
+            elif c == 0:
+                done = True
+
+        self.todolist.jobs[i] = job
+        self.todolist.Sort()
+        self.Autosave()
+
+    def cmdSave(self, args):
+        if len(args) > 1:
+            path = args[1]
+        else:
+            path = input("Path to save to:")
+        self.todolist.Save(path)
+
+    def cmdFile(self, path):
+        self.todolist.Load(path)
+
+    def cmdList(self):
+        for i, j in enumerate(self.todolist.jobs):
+            print(i, "| ", j.name, j.deadline.strftime(self.dateformat))
+
+    def cmdSearch(self, args):
+        args.pop(0)
+        idx = self.todolist.SearchTags(args)
+        for i in idx:
+            print(i, "| ", self.todolist.jobs[i].name, self.todolist.jobs[i].deadline.strftime(self.dateformat))
+
+    def cmdDetail(self, i):
+        j = self.todolist.jobs[i]
+        print(i, "| ", j.name)
+        print(j.deadline.strftime(self.dateformat))
+        print(j.description)
+        print(j.tags)
+        print(j.status.name)
+
+    def Autosave(self):
+        if self.autosave:
+            self.todolist.Save(self.todolist.loadedpath)
 
 if __name__ == "__main__":
-    main()
+    i = Interface()
+    i.Run()
